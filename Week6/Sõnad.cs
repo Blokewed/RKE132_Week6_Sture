69883a06fa735c443a12c18227127c3374a33851
Blokewed <peeter.sture@tktk.ee>
2024-12-06 21:01:31 +0200

    Week6

---
+++ Week6/Sõnad.cs
@@ -0,0 +1,29 @@
+﻿string hello = " hello, world! Let`s just add more words and see what happens. ";
+
+int stringLenght = hello.Length;   
+
+//string trimmedString  = hello.Trim();
+
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0]));
+
+firstLetter = hello[0];
+
+
+Console.WriteLine(firstLetter);
+
+//int wordCounter = 0;
+
+//for (int i = 0; i < hello.Length ; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//{
+//    wordCounter++;
+//}
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter + 1} words");
